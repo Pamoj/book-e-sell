1d45956e6daec135426bdf84906a53b7b1c2260c
Pamoj <101511453+Pamoj@users.noreply.github.com>
2022-07-09 00:27:53 +0530

    Add files via upload

---
+++ BookStoreRepository/OrderdtlRepository.cs
@@ -0,0 +1,47 @@
+﻿using BookStore.Repository;
+using BookStoreModels.NewFolder;
+using BookStoreModels.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreRepository
+{
+    public class OrderdtlRepository : BaseRepository
+    {
+        public Orderdtl GetOrderdtl(int id)
+        {
+            return _context.Orderdtls.FirstOrDefault(c => c.Id == id);
+        }
+
+        public Orderdtl AddOrderdtl(Orderdtl category)
+        {
+            try
+            {
+                var entry = _context.Orderdtls.Add(category);
+                _context.SaveChanges();
+                return entry.Entity;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            
+        }
+
+
+        public bool DeleteOrderdtl(int id)
+        {
+            var category = _context.Orderdtls.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+                return false;
+
+            _context.Orderdtls.Remove(category);
+            _context.SaveChanges();
+            return true;
+        }
+    }
+}
+
---
+++ BookStoreRepository/OrdermstRepository.cs
@@ -0,0 +1,37 @@
+﻿using BookStore.Repository;
+using BookStoreModels.NewFolder;
+using BookStoreModels.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreRepository
+{
+    public class OrdermstRepository : BaseRepository
+    {
+        public Ordermst GetOrderMst(int id)
+        {
+            return _context.Ordermsts.FirstOrDefault(c => c.Id == id);
+        }
+
+        public Ordermst AddOrderMsts(Ordermst category)
+        {
+            var entry = _context.Ordermsts.Add(category);
+            _context.SaveChanges();
+            return entry.Entity;
+        }
+
+        public bool DeleteOrderMst(int id)
+        {
+            var category = _context.Ordermsts.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+                return false;
+
+            _context.Ordermsts.Remove(category);
+            _context.SaveChanges();
+            return true;
+        }
+    }
+}
