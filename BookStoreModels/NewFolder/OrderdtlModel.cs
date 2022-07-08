1209e79d4354b07f365f6e0112d653145b444f20
Pamoj <101511453+Pamoj@users.noreply.github.com>
2022-07-09 00:25:19 +0530

    Add files via upload

---
+++ BookStoreModels/NewFolder/OrderdtlModel.cs
@@ -0,0 +1,29 @@
+﻿using BookStoreModels.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreModels.NewFolder
+{
+    public class OrderdtlModel
+    {
+        public OrderdtlModel() { }
+
+        public OrderdtlModel(Orderdtl order)
+        {
+            Id = order.Id;
+            Bookid = order.Bookid;
+            Quantity = order.Quantity;
+            Ordermstid = order.Ordermstid;
+        }
+
+        public int Id { get; set; }
+        public int? Bookid { get; set; }
+        public int? Quantity { get; set; }
+        public decimal? Totalprice { get; set; }
+        public int? Ordermstid { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
---
+++ BookStoreModels/NewFolder/OrdermstModel.cs
@@ -0,0 +1,26 @@
+﻿using BookStoreModels.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreModels.NewFolder
+{
+    public class OrdermstModel
+    {
+        public OrdermstModel() { }
+
+        public OrdermstModel(Ordermst order)
+        {
+            Id = order.Id;
+            Userid = order.Userid;
+            Orderdate = order.Orderdate;
+            Totalprice = order.Totalprice;
+        }
+        public int Id { get; set; }
+        public int? Userid { get; set; }
+        public DateTime? Orderdate { get; set; }
+        public decimal? Totalprice { get; set; }
+    }
+}
